fd3ad7f14c7d01d4eee83acd94fcffca1097bf6e
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2024-08-07 16:08:00 -0300

    Configure GitHub repositories

--- github_mirror.tf
+++ github_mirror.tf
@@ -2,13 +2,22 @@ resource github_repository main {
 	count = var.github_mirror != null ? 1 : 0
 	
 	name = var.github_mirror.name
+	is_template = var.github_mirror.is_template
 	description = var.description
 	homepage_url = var.github_mirror.homepage_url
-	is_template = var.github_mirror.is_template
+	topics = var.topics
 	
 	has_issues = true
 	has_projects = false
 	has_wiki = false
+}
+
+
+resource github_actions_repository_permissions main {
+	count = var.github_mirror != null ? 1 : 0
+	
+	repository = github_repository.main[0].name
+	enabled = false
 }
 
 
@@ -17,5 +26,5 @@ resource gitlab_project_mirror main {
 	
 	project = gitlab_project.main.id
 	# TODO: Fix this.
-	url = replace( github_repository.main[0].http_clone_url, "/\\/\\//", "//Marcelotsvaz:password@" )
+	url = replace( github_repository.main[0].http_clone_url, "/\\/\\//", "//git:password@" )
 }
